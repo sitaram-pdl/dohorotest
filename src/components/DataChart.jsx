0343e66eec69a26ca75d042b56ef114274171b3f
Sitaram <spoudel646@gmail.com>
2024-07-13 19:12:18 +0545

    NEW : Adds charts and integrate with table

--- src/components/DataChart.jsx
+++ src/components/DataChart.jsx
@@ -1,38 +1,58 @@
-import { PieChart, Pie, ResponsiveContainer } from 'recharts';
+import {
+  PieChart,
+  Pie,
+  Tooltip,
+  Legend,
+  Cell,
+  ResponsiveContainer,
+} from 'recharts';
+import calculateNetWeight from '../lib/calculateNetWeight';
+import { useSelector } from 'react-redux';
 
-const data01 = [
-  { name: 'Group A', value: 400 },
-  { name: 'Group B', value: 300 },
-  { name: 'Group C', value: 300 },
-  { name: 'Group D', value: 200 },
-];
-const data02 = [
-  { name: 'A1', value: 100 },
-  { name: 'A2', value: 300 },
-  { name: 'B1', value: 100 },
-  { name: 'B2', value: 80 },
-  { name: 'B3', value: 40 },
-  { name: 'B4', value: 30 },
-  { name: 'B5', value: 50 },
-  { name: 'C1', value: 100 },
-  { name: 'C2', value: 200 },
-  { name: 'D1', value: 150 },
-  { name: 'D2', value: 50 },
-];
+const normalizeData = (array) => {
+  return array.map((item) => {
+    const netWeight = parseFloat(calculateNetWeight(item).split(' ')[0]);
 
-const DataCharts = () => (
-  <PieChart width={400} height={400}>
-    <Pie
-      data={data02}
-      dataKey='value'
-      cx='50%'
-      cy='50%'
-      innerRadius={70}
-      outerRadius={90}
-      fill='#82ca9d'
-      label
-    />
-  </PieChart>
-);
+    return {
+      name: item['Waste Name'],
+      value: isNaN(netWeight) ? 2 : netWeight,
+    };
+  });
+};
+
+function getRandomColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+const DataCharts = () => {
+  const items = useSelector((state) => state.items);
+  const data = normalizeData(items);
+
+  const COLORS = items.map(() => getRandomColor());
+
+  return (
+    <div className='lg:w-1/2'>
+      <ResponsiveContainer width='100%' height={400}>
+        <PieChart>
+          <Pie data={data} labelLine={false} fill='#8884d8' dataKey='value'>
+            {data.map((entry, index) => (
+              <Cell
+                key={`cell-${index}`}
+                fill={COLORS[index % COLORS.length]}
+              />
+            ))}
+          </Pie>
+          <Tooltip />
+          <Legend />
+        </PieChart>
+      </ResponsiveContainer>
+    </div>
+  );
+};
 
 export default DataCharts;
